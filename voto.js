0477a89da0b972a28d0aec880b70709c1aa15d8c
Mauricio <mauricio.p.raupp@gmail.com>
2024-03-08 11:25:23 -0300

    atividade finalizada

---
+++ voto.js
@@ -0,0 +1,9 @@
+const idade = parseInt(17);
+
+if (idade >= 16 & idade < 18 || idade >= 70){
+  console.log("Voto facultativo")
+} else if (idade >= 18 && idade < 70){
+  console.log("Voto obrigatório")
+} else {
+  console.log("Não possui idade para votar")
+};
